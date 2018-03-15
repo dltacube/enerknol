36130212c4945ab84cf1e56bab27d9f911913dbe
Unknown <jrevah@gmail.com>
2018-03-15 02:16:22 -0400

    enhancing app object
    Adding elasticsearch and pymongo instances to our app object

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,24 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
+
 login = LoginManager(app)
 login.login_view = 'login'
+
+mongo = PyMongo(app)
+app.elasticsearch = Elasticsearch([app.config['ELASTIC_SEARCH_URL']]) if app.config['ELASTIC_SEARCH_URL'] else None
 
 # Avoids circular imports
 from app import routes, models
