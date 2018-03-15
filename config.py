caf00eb83585686c037841c3e55ea7133f5b2e7d
Unknown <jrevah@gmail.com>
2018-03-15 14:23:43 -0400

    small notes

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    # eventually would be used for generating hashes
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dkfshgkjehkijugth345'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
