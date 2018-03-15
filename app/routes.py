51d34e0b17649055a86adb7cc93b011cfadce5b5
[heyoni] <jrevah@gmail.com>
2018-03-15 12:03:25 -0400

    added custom filter that adds <mark> tags to search terms

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
+from jinja2 import Markup, escape
 from werkzeug.urls import url_parse
 from app.models import User, Movies
 from . import app, db
 from .forms import LoginForm, RegistrationForm, SearchForm
+import re
 
 
 @app.route('/')
@@ -65,3 +67,15 @@ def search():
     next_url = url_for('search', page=page + 1, q=query) if page * per_page < total else None
     prev_url = url_for('search', page=page - 1, q=query) if page > 1 else None
     return render_template('results.html', movies=movies, next_url=next_url, prev_url=prev_url)
+
+def mark(value):
+    # fetch the search term
+    q = request.args.get('q')
+    regx = re.compile(r"("+ q + ")", flags=re.IGNORECASE)
+    esc_val = escape(value)
+    # replace search term w/ mark tags
+    result = re.sub(regx, r'<mark>\1</mark>', str(esc_val))
+    # Make sure the html tags are not escaped
+    return Markup(result)
+# register the filter w/ the jinja environment
+app.jinja_env.filters['mark'] = mark
